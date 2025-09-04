8add75a91abb6fde024b1ea8bf39486a81ccfaf1
seixasxbr <mateus.seixas@tidewise.io>
2025-09-04 14:26:19 -0300

    feat: add method to process the read buffer

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -7,6 +7,7 @@ using namespace sonar_oculus_m750d;
 Driver::Driver()
     : iodrivers_base::Driver::Driver(INTERNAL_BUFFER_SIZE)
 {
+    m_protocol = Protocol();
 }
 
 int Driver::extractPacket(uint8_t const* buffer, size_t buffer_size) const
@@ -19,17 +20,24 @@ int Driver::extractPacket(uint8_t const* buffer, size_t buffer_size) const
     OculusMessageHeader header;
     memcpy(&header, buffer, sizeof(OculusMessageHeader));
     if (header.oculusId != OCULUS_CHECK_ID) {
-        // Invalid data in the header - flush the buffer
+        // Invalid data in the header - // discard first byte
         return -1;
     }
 
     size_t packet_size = sizeof(OculusMessageHeader) + header.payloadSize;
-    if (buffer_size >= packet_size)
-    {
+    if (buffer_size >= packet_size) {
         // Packet starting at zero is valid
         return packet_size;
     }
 
     // Not enough bytes for a valid packet - wait for new bytes
     return 0;
+}
+
+base::samples::Sonar Driver::processOne()
+{
+    int packet_size = readPacket(m_read_buffer, INTERNAL_BUFFER_SIZE);
+    m_protocol.handleBuffer(m_read_buffer, packet_size);
+    auto sonar = m_protocol.parseSonar();
+    return sonar;
 }
--- src/Driver.hpp
+++ src/Driver.hpp
@@ -1,15 +1,20 @@
 #ifndef SONAR_OCULUS_M750D_DRIVER_HPP
 #define SONAR_OCULUS_M750D_DRIVER_HPP
 
+#include "Protocol.hpp"
 #include "iodrivers_base/Driver.hpp"
+#include <base/samples/Sonar.hpp>
 
 namespace sonar_oculus_m750d {
+    static const int INTERNAL_BUFFER_SIZE = 200000;
     class Driver : iodrivers_base::Driver {
-        static const int INTERNAL_BUFFER_SIZE = 200000;
 
     public:
         Driver();
         int extractPacket(uint8_t const* buffer, size_t buffer_size) const;
+        base::samples::Sonar processOne();
+        uint8_t m_read_buffer[INTERNAL_BUFFER_SIZE];
+        Protocol m_protocol;
     };
 }
 
